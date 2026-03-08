9582fe98a8f186e8b07d893133a1abe0b40172a3
DavDem <davidedemarco088@gmail.com>
2026-03-08 14:32:23 +0000

    a

--- Lista.java
+++ Lista.java
@@ -113,4 +113,28 @@ public class Lista<T> {
         }
         return indice;
     }
+    public boolean cancella(T dato) {
+        if (head == null) {
+            return false;
+        }
+        
+        if (head.dato.equals(dato)) {
+            head = head.next;
+            return true;
+        }
+        
+        Nodo<T> precedente = head;
+        Nodo<T> corrente = head.next;
+        
+        while (corrente != null) {
+            if (corrente.dato.equals(dato)) {
+                precedente.next = corrente.next;
+                return true;
+            }
+            precedente = corrente;
+            corrente = corrente.next;
+        }
+        
+        return false;
+    }
 }
